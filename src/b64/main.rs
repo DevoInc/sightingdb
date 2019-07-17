02d06e3423c959e94ca823ec974fc65638de0e29
Sebastien Tricaud <sebastien.tricaud@gmail.com>
2019-07-17 09:16:12 -0700

    Adding the b64 tool, which translates the argument into a base64 url with no padding

---
+++ src/b64/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::process;
+
+extern crate base64;
+use base64::{encode_config, URL_SAFE_NO_PAD};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Syntax: {} string", &args[0]);
+        process::exit(1);
+    }
+    
+    let strenc = &args[1];
+    let encoded_val = encode_config(&strenc, URL_SAFE_NO_PAD);
+
+    println!("{}", encoded_val);
+}
