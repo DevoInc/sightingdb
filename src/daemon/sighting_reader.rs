e3b5dccc5da99459c20a8c136e67430f160ce25b
Sebastien Tricaud <sebastien.tricaud@gmail.com>
2019-11-04 23:59:06 -0800

    Can now read/write a value without forcing it to be base64, less code? -> less bugs.

--- src/daemon/sighting_reader.rs
+++ src/daemon/sighting_reader.rs
@@ -9,20 +9,17 @@ pub struct Message {
 }
 
 pub fn read(db: &mut Database, path: &str, value: &str) -> String {
-    let decoded_val = decode_config(&value, URL_SAFE_NO_PAD);
-    match decoded_val {
-        Ok(b64_val) => {
-            let str_val = std::str::from_utf8(&b64_val).unwrap();
-
-            
-            
-            return db.get_attr(path, str_val);
-        },
-        Err(..) => {
-            println!("Read Error: Reading base64 input");
-            let err = serde_json::to_string(&Message{message: String::from("Invalid base64 encoding (base64 url with non padding) value")});
-            return err.unwrap();
-        },
-    }
+    // let decoded_val = decode_config(&value, URL_SAFE_NO_PAD);
+    // match decoded_val {
+    //     Ok(b64_val) => {
+    //         let str_val = std::str::from_utf8(&b64_val).unwrap();
+    return db.get_attr(path, value);
+    //     },
+    //     Err(..) => {
+    //         println!("Read Error: Reading base64 input");
+    //         let err = serde_json::to_string(&Message{message: String::from("Invalid base64 encoding (base64 url with non padding) value")});
+    //         return err.unwrap();
+    //     },
+    // }
     
 }
--- src/daemon/sighting_writer.rs
+++ src/daemon/sighting_writer.rs
@@ -1,19 +1,19 @@
-extern crate base64;
-use base64::{decode_config, encode_config, URL_SAFE_NO_PAD};
+// extern crate base64;
+// use base64::{decode_config, encode_config, URL_SAFE_NO_PAD};
 
 use crate::db::Database;
 
 pub fn write(db: &mut Database, path: &str, value: &str) -> bool {
-    let decoded_val = decode_config(&value, URL_SAFE_NO_PAD);
-    match decoded_val {
-        Ok(b64_val) => {
-            let str_val = std::str::from_utf8(&b64_val).unwrap();
-            db.write(path, str_val);
-            return true;
-        },
-        Err(..) => {
-            println!("Write Error: Reading base64 input");
-            return false;
-        },
-    }
+    // let decoded_val = decode_config(&value, URL_SAFE_NO_PAD);
+    // match decoded_val {
+    //     Ok(b64_val) => {
+            // let str_val = std::str::from_utf8(&b64_val).unwrap();
+    db.write(path, value);
+    return true;
+    //     },
+    //     Err(..) => {
+    //         println!("Write Error: Reading base64 input");
+    //         return false;
+    //     },
+    // }
 }
